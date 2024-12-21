b9b3c1bc0a0c8dcbe153e5318f37a30ae9aa3c1b
Hristo <whatickata10@gmail.com>
2024-12-21 19:30:15 +0200

    added posts and menubar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,30 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: {
+      user: {
+        select: {
+          username: true,
+          displayName: true,
+          avatarUrl: true,
+        },
+      },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <main>Front Page</main>
+    <main className="h-[200vh] w-full">
+      <div className="w-full">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
   );
 }
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -9,11 +9,15 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
+  DropdownMenuSubTrigger,
+  DropdownMenuPortal,
 } from "./ui/dropdown-menu";
 import Link from "next/link";
-import { LogOutIcon, UserIcon } from "lucide-react";
+import { Check, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
 import { logout } from "@/app/(auth)/actions";
 import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+import { DropdownMenuSub, DropdownMenuSubContent } from "@radix-ui/react-dropdown-menu";
 
 interface UserButtonProps {
   className?: string;
@@ -21,6 +25,8 @@ interface UserButtonProps {
 
 export default function UserButton({ className }: UserButtonProps) {
   const { user } = useSession();
+
+  const {theme, setTheme} = useTheme();
 
   return (
     <DropdownMenu>
@@ -38,6 +44,30 @@ export default function UserButton({ className }: UserButtonProps) {
             Profile
           </DropdownMenuItem>
         </Link>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Monitor className="mr-2 size-4" />
+            Theme
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System default
+                {theme === "system" && <Check className="ms-2 size-4"/>}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                <Sun className="mr-2 size-4"/>
+                Light
+                {theme === "light" && <Check className="ms-2 size-4"/>}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <Moon className="mr-2 size-4"/>
+                Dark
+                {theme === "dark" && <Check className="ms-2 size-4"/>}
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
         <DropdownMenuSeparator />
         <DropdownMenuItem
           onClick={logout}
