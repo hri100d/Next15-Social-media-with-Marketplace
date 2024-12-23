959318c794c609e817c916dffc54a0bc8acf817c
Hristo <whatickata10@gmail.com>
2024-12-23 18:51:31 +0200

    added ability to delete posts, ability to follow other users and following feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,20 +1,26 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany({
-    include: postDataInclude,
-    orderBy: { createdAt: "desc" },
-  });
-
   return (
     <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,17 +1,17 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
+import { getUserDataSelect } from "@/lib/types";
+import { formatNumber } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { unstable_cache } from "next/cache";
 import Link from "next/link";
 import { Suspense } from "react";
+import FollowButton from "./FollowButton";
 import UserAvatar from "./UserAvatar";
-import { Button } from "./ui/button";
-import { unstable_cache } from "next/cache";
-import { formatNumber } from "@/lib/utils";
 
 export default function TrendsSidebar() {
   return (
-    <div className="sticky top-[5.25rem] hidden md:block lg:w-80 w-72 flex-none space-y-5 h-fit">
+    <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <WhoToFollow />
         <TrendingTopics />
@@ -28,16 +28,21 @@ async function WhoToFollow() {
   const usersToFollow = await prisma.user.findMany({
     where: {
       NOT: {
-        id: user?.id,
+        id: user.id,
+      },
+      followers: {
+        none: {
+          followerId: user.id,
+        },
       },
     },
-    select: userDataSelect,
+    select: getUserDataSelect(user.id),
     take: 5,
   });
 
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
-      <div className="text-x1 font-bold">Who to follow</div>
+      <div className="text-xl font-bold">Who to follow</div>
       {usersToFollow.map((user) => (
         <div key={user.id} className="flex items-center justify-between gap-3">
           <Link
@@ -54,7 +59,16 @@ async function WhoToFollow() {
               </p>
             </div>
           </Link>
-          <Button>Follow</Button>
+
+          <FollowButton
+            userId={user.id}
+            initialState={{
+              followers: user._count.followers,
+              isFollowedByUser: user.followers.some(
+                ({ followerId }) => followerId === user.id
+              ),
+            }}
+          />
         </div>
       ))}
     </div>
