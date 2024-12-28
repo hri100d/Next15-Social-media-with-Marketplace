d28c1c0602b4829002419b380db7642d0c2aee48
Hristo <whatickata10@gmail.com>
2024-12-28 19:12:11 +0200

    added chats

--- src/app/(auth)/signup/actions.ts
+++ src/app/(auth)/signup/actions.ts
@@ -8,6 +8,7 @@ import { isRedirectError } from "next/dist/client/components/redirect";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { hash } from "argon2";
+import streamServerClient from "@/lib/stream";
 
 export async function signUp(
   credentials: SignUpValues
@@ -53,14 +54,22 @@ export async function signUp(
       };
     }
 
-    await prisma.user.create({
-      data: {
+    await prisma.$transaction(async (tx) => {
+      await tx.user.create({
+        data: {
+          id: userId,
+          username,
+          displayName: username,
+          email,
+          passwordHash,
+        },
+      });
+
+      await streamServerClient.upsertUser({
         id: userId,
         username,
-        displayName: username,
-        email,
-        passwordHash,
-      },
+        name: username,
+      });
     });
 
     const session = await lucia.createSession(userId, {});
